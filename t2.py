7650c2bdc4f16b105c54e30f4af5a804896f93b6
Alena_Drevina <alena_drevina@epam.com>
2022-11-03 17:38:19 +0100

    connection2

--- t2.py
+++ t2.py
@@ -1,7 +1,5 @@
+import pyodbc
 import pytest
 
-try:
-    connection = pyodbc.connect('DRIVER={SQL Server};Server=EPBYMINW0EE0\\SQLEXPRESS03;Database=TRN;username= testuser;password=password12345')
-    print("There is connection to DB")
-except Exception as ex:
-    print(ex)
+connection = pyodbc.connect('DRIVER={SQL Server};Server=EPBYMINW0EE0\\SQLEXPRESS03;Database=TRN;username= testuser;password=password12345')
+print("There is connection to DB")

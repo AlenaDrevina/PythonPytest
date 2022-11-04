c8103e9bb74a786e7a8fb78a5a8e2c38760c7171
AlenaDrevina <117280406+AlenaDrevina@users.noreply.github.com>
2022-11-04 10:27:19 +0100

    Update t3.py

--- t3.py
+++ t3.py
@@ -1,5 +1,2 @@
-import pyodbc
-import pytest
-
 connection = pyodbc.connect('DRIVER={SQL Server};Server=EPBYMINW0EE0\\SQLEXPRESS03;Database=TRN;username= testuser;password=password12345')
 print("There is connection to DB")
